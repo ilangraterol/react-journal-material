061f214f73118eea0a8e45c29bd6da48598ef160
Ilan Graterol <ilan.graterol@hotmail.com>
2022-11-03 13:52:10 -0500

    first commit

---
+++ src/firebase/config.js
@@ -0,0 +1,48 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore/lite';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD4I_3HZCynNvwDOVqKEIbX0MQha0rijsU",
+  authDomain: "journal-react-fc536.firebaseapp.com",
+  projectId: "journal-react-fc536",
+  storageBucket: "journal-react-fc536.appspot.com",
+  messagingSenderId: "30979177189",
+  appId: "1:30979177189:web:b67f590adf9e253aee720c"
+};
+
+// Initialize Firebase
+export const FirebaseApp = initializeApp(firebaseConfig);
+export const FirebaseAuth = getAuth(FirebaseApp);
+export const FirebaseDB   = getFirestore( FirebaseApp );
+
+
+
+/*
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore/lite';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC9qM8LKYti4TZus0g00ISzMjudet-H2vg",
+  authDomain: "react-cursos-4368a.firebaseapp.com",
+  projectId: "react-cursos-4368a",
+  storageBucket: "react-cursos-4368a.appspot.com",
+  messagingSenderId: "35683178704",
+  appId: "1:35683178704:web:2cab2003319bbac2eba6d5"
+};
+
+// Initialize Firebase
+export const FirebaseApp  = initializeApp(firebaseConfig);
+export const FirebaseAuth = getAuth( FirebaseApp );
+export const FirebaseDB   = getFirestore( FirebaseApp );
+
+**/
